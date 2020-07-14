21358936b29c2c0bfcaade5bc4096a1ee9ca49c4
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-07-14 21:36:51 +0100

    preparing to allow building of multiple files

--- default/page.go
+++ default/page.go
@@ -2,12 +2,20 @@ package main
 
 import "github.com/SamtheSaint/jamgo/tools"
 
-// PageData supples data for the page to parse
+// PageData supples data for the page to parse.
+// Parses {folderName}.gohtml template and
+// is stored in the root directory of the build directory
 var PageData tools.Page
+
+// PageDataCollection is used to generate multiple pages from the same template
+// uses template {folderName}_multiple.gohtml and is stored in
+// {buildDir}/{folderName}
+var PageDataCollection []tools.Page
 
 func init() {
 	PageData = tools.Page{
 		Title: "Enter Title Here",
 		Data:  nil,
 	}
+	PageDataCollection = nil
 }
